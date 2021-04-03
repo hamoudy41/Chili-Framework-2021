85013c5f2a48f4443ceaa43b083542a8787e3a89
hamoudy41 <hamoudy41@yahoo.com>
2021-04-03 14:37:39 +0200

    Setting up game pixels

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -42,6 +42,72 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
-	gfx.PutPixel( 100, 100, 255, 0, 0 );
-	gfx.
+	int x = 400;
+	int y = 300;
+	int gb = 255;
+
+	if(wnd.kbd.KeyIsPressed(VK_RIGHT))
+	{
+		x = x + 100;
+	}
+
+	if(wnd.kbd.KeyIsPressed(VK_LEFT))
+	{
+		x = x - 100;
+	}
+
+	if(wnd.kbd.KeyIsPressed(VK_DOWN))
+	{
+		y = y + 100;
+	}
+
+	if(wnd.kbd.KeyIsPressed(VK_UP))
+	{
+		y = y - 100;
+	}
+
+	if(wnd.kbd.KeyIsPressed(VK_CONTROL))
+	{
+		gb = 0;
+	}
+
+	if(wnd.kbd.KeyIsPressed( VK_SHIFT))
+	{
+		gfx.PutPixel(-5 + x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(-5 + x, -4 + y, 255, gb, gb);
+		gfx.PutPixel(-5 + x, -3 + y, 255, gb, gb);
+		gfx.PutPixel(-4 + x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(-3 + x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(-5 + x, 5 + y, 255, gb, gb);
+		gfx.PutPixel(-5 + x, 4 + y, 255, gb, gb);
+		gfx.PutPixel(-5 + x, 3 + y, 255, gb, gb);
+		gfx.PutPixel(-4 + x, 5 + y, 255, gb, gb);
+		gfx.PutPixel(-3 + x, 5 + y, 255, gb, gb);
+		gfx.PutPixel(5 + x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(5 + x, -4 + y, 255, gb, gb);
+		gfx.PutPixel(5 + x, -3 + y, 255, gb, gb);
+		gfx.PutPixel(4 + x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(3 + x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(5 + x, 5 + y, 255, gb, gb);
+		gfx.PutPixel(5 + x, 4 + y, 255, gb, gb);
+		gfx.PutPixel(5 + x, 3 + y, 255, gb, gb);
+		gfx.PutPixel(4 + x, 5 + y, 255, gb, gb);
+		gfx.PutPixel(3 + x, 5 + y, 255, gb, gb);
+	}
+	else
+	{
+		gfx.PutPixel(-5 + x, y, 255, gb, gb);
+		gfx.PutPixel(-4 + x, y, 255, gb, gb);
+		gfx.PutPixel(-3 + x, y, 255, gb, gb);
+		gfx.PutPixel(5 + x, y, 255, gb, gb);
+		gfx.PutPixel(4 + x, y, 255, gb, gb);
+		gfx.PutPixel(5 + x, y, 255, gb, gb);
+		gfx.PutPixel(x, -5 + y, 255, gb, gb);
+		gfx.PutPixel(x, -4 + y, 255, gb, gb);
+		gfx.PutPixel(x, -3 + y, 255, gb, gb);
+		gfx.PutPixel(x, 5 + y, 255, gb, gb);
+		gfx.PutPixel(x, 4 + y, 255, gb, gb);
+		gfx.PutPixel(x, 5 + y, 255, gb, gb);
+	}
+	
 }
